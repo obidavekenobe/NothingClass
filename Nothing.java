622d9fd4c275bc3fea95d8ee3b2c9160222e02c8
obidavekenobe <58524923+obidavekenobe@users.noreply.github.com>
2023-07-12 11:46:15 -0700

    Add files via upload

---
+++ Nothing.java
@@ -0,0 +1,21 @@
+public class Nothing {
+
+	boolean work;
+
+	public Nothing() {
+		work = true;
+	}
+
+public static void main(String[] args) {
+
+	Nothing natta = new Nothing();
+
+	do {
+		System.out.println( "Nothing" + '\n' );
+	}
+	while ( natta.work = true ); 
+
+	} //END Nothing()
+
+} //END main()
+
